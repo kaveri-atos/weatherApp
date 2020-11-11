feac684c87509b28206755700837c0c0c9b291b0
namitapatil0502 <namita.patil@atos.net>
2020-11-11 19:02:04 +0530

    Added changes for responsive webpage and multiple env configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EnvironmentService } from './services/environment.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather-app';
+
+  constructor(private env : EnvironmentService){
+    console.log("Environment selected : "+JSON.stringify(env.envConfig))
+  }
 }
